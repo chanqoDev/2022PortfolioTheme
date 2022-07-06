001a1a32155866e0b8485405b330c360237a5131
Christian E <christianelena92@gmail.com>
2022-07-06 11:04:51 -0500

    import avt pngs

--- src/assets/fake-data/data-top-seller.js
+++ src/assets/fake-data/data-top-seller.js
@@ -1,91 +1,105 @@
-import img1 from '../images/avatar/avt-1.jpg'
-import img2 from '../images/avatar/avt-2.jpg'
-import img3 from '../images/avatar/avt-3.jpg'
-import img4 from '../images/avatar/avt-4.jpg'
-import img5 from '../images/avatar/avt-5.jpg'
-import img6 from '../images/avatar/avt-6.jpg'
-import img7 from '../images/avatar/avt-7.jpg'
-import img8 from '../images/avatar/avt-8.jpg'
-import img9 from '../images/avatar/avt-9.jpg'
+import img1 from '../images/avatar/avt-1.png';
+import img2 from '../images/avatar/avt-2.png';
+// import img3 from '../images/avatar/avt-3.jpg';
+import img4 from '../images/avatar/avt-4.jpg';
+import img5 from '../images/avatar/avt-5.png';
+import img6 from '../images/avatar/avt-6.png';
+import img7 from '../images/avatar/avt-7.png';
+import img8 from '../images/avatar/avt-8.png';
+import img9 from '../images/avatar/avt-9.png';
+import img10 from '../images/avatar/avt-10.png';
+import img11 from '../images/avatar/avt-11.png';
+import img12 from '../images/avatar/avt-12.png';
+import img13 from '../images/avatar/avt-13.png';
+import img14 from '../images/avatar/avt-14.png';
 
-
-
+import imgx from '../images/avatar/avt-3.png';
 
 const topSellerData = [
-    {
-        img: img1,
-        name: "Crispin Berry" ,
-        price: "214.2 ETH",
-        classPadding: ""
-    },
-    {
-        img: img2,
-        name: "Samson Frost" ,
-        price: "205.43 ETH",
-        classPadding: "pl-17"
-    },
-    {
-        img: img4,
-        name: "Tommy Alrez" ,
-        price: "170.3 ETH",
-        classPadding: "pl-34"
-    },
-    {
-        img: img5,
-        name: "Windsor Lane" ,
-        price: "120.7 ETH",
-        classPadding: "pl-51"
-    },
-    {
-        img: img3,
-        name: "Andy Hurlbutt" ,
-        price: "82.79 ETH",
-        classPadding: "pl-68"
-    },
-    {
-        img: img8,
-        name: "Blake Banks" ,
-        price: "68.2 ETH",
-        classPadding: ""
-    },
-    {
-        img: img6,
-        name: "Monica Lucas" ,
-        price: "52.8 ETH",
-        classPadding: "pl-17"
-    },
-    {
-        img: img9,
-        name: "Matt Ramos" ,
-        price: "38.4 ETH",
-        classPadding: "pl-34"
-    },
-    {
-        img: img7,
-        name: "Harper Wilcher" ,
-        price: "29.2 ETH",
-        classPadding: "pl-51"
-    },
-    {
-        img: img1,
-        name: "Crispin Berry" ,
-        price: "214.2 ETH",
-        classPadding: "pl-68"
-    },
-    {
-        img: img1,
-        name: "Crispin Berry" ,
-        price: "214.2 ETH",
-        classPadding: ""
-    },
-    {
-        img: img2,
-        name: "Samson Frost" ,
-        price: "205.43 ETH",
-        classPadding: "pl-17"
-    },
-
-
-]
+  {
+    img: img1,
+    name: 'HTML5',
+    // price: "214.2 ETH",
+    classPadding: '',
+  },
+  {
+    img: img2,
+    name: 'CSS3',
+    price: 'Flexbox, Grids',
+    classPadding: 'pl-17',
+  },
+  {
+    img: img5,
+    name: 'Javascript',
+    // price: '170.3 ETH',
+    classPadding: 'pl-34',
+  },
+  {
+    img: img14,
+    name: 'Redux',
+    // price: '120.7 ETH',
+    classPadding: 'pl-51',
+  },
+  {
+    img: imgx,
+    name: 'Node.js',
+    // price: '82.79 ETH',
+    classPadding: 'pl-68',
+  },
+  {
+    img: img8,
+    name: 'Yarn',
+    // price: '68.2 ETH',
+    classPadding: '',
+  },
+  {
+    img: img6,
+    name: 'React.js',
+    // price: '52.8 ETH',
+    classPadding: 'pl-17',
+  },
+  {
+    img: img10,
+    name: 'Firebase',
+    // price: '38.4 ETH',
+    classPadding: 'pl-34',
+  },
+  {
+    img: img7,
+    name: 'TS',
+    // price: '29.2 ETH',
+    classPadding: 'pl-51',
+  },
+  {
+    img: img9,
+    name: 'MaterialUI',
+    // price: '214.2 ETH',
+    classPadding: 'pl-68',
+  },
+  {
+    img: img11,
+    name: 'Serverless',
+    // price: '214.2 ETH',
+    classPadding: '',
+  },
+  {
+    img: img12,
+    name: 'MongoDB',
+    // price: '205.43 ETH',
+    classPadding: 'pl-17',
+  },
+  {
+    img: img4,
+    name: 'JQuery',
+    // price: '205.43 ETH',
+    classPadding: 'pl-17',
+  },
+  {
+    img: img13,
+    name: 'NPM',
+    // price: '205.43 ETH',
+    classPadding: 'pl-17',
+  },
+];
 
 export default topSellerData;
