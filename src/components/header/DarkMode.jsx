ad056ed11a268ad3a474d3851c869812d5bee15f
Christian E <christianelena92@gmail.com>
2022-07-06 03:39:48 -0500

    Import: logos for light and dark theme and nav styling

--- src/components/header/DarkMode.jsx
+++ src/components/header/DarkMode.jsx
@@ -1,46 +1,48 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import imgsun from '../../assets/images/icon/sun.png'
-
+import { Link } from 'react-router-dom';
+// import imgsun from '../../assets/images/icon/sun.png';
+import imgMoon from '../../assets/images/icon/moon.png';
 const DarkMode = () => {
-    let clickedClass = "clicked"
-    const body = document.body
-    const lightTheme = "light"
-    const darkTheme = "is_dark"
-    let theme
+  let clickedClass = 'clicked';
+  const body = document.body;
+  const lightTheme = 'light';
+  const darkTheme = 'is_dark';
+  let theme;
 
-    if (localStorage) {
-        theme = localStorage.getItem("theme")
-    }
-    if (theme === lightTheme || theme === darkTheme) {
-        body.classList.add(theme)
+  if (localStorage) {
+    theme = localStorage.getItem('theme');
+  }
+  if (theme === lightTheme || theme === darkTheme) {
+    body.classList.add(theme);
+  } else {
+    body.classList.add(darkTheme);
+  }
+
+  const switchTheme = (e) => {
+    if (theme === darkTheme) {
+      body.classList.replace(darkTheme, lightTheme);
+      e.target.classList.remove(clickedClass);
+      localStorage.setItem('theme', 'light');
+
+      theme = lightTheme;
     } else {
-        body.classList.add(darkTheme)
-    }
+      body.classList.replace(lightTheme, darkTheme);
+      e.target.classList.add(clickedClass);
+      localStorage.setItem('theme', 'is_dark');
 
-    const switchTheme = e => {
-        if (theme === darkTheme) {
-            body.classList.replace(darkTheme, lightTheme)
-            e.target.classList.remove(clickedClass)
-            localStorage.setItem("theme", "light")
-            theme = lightTheme
-        } else {
-            body.classList.replace(lightTheme, darkTheme)
-            e.target.classList.add(clickedClass)
-            localStorage.setItem("theme", "is_dark")
-            theme = darkTheme
-        }
+      theme = darkTheme;
     }
-    return (
-        <div className="mode_switcher">
-        <h6>Dark mode <strong>Available</strong></h6>
-        <Link to="#" 
-            onClick={e => switchTheme(e)}  >
-            <img src={imgsun} alt="" />
-        </Link>
-
+  };
+  return (
+    <div className="mode_switcher">
+      <h6>
+        Dark mode <strong>Available</strong>
+      </h6>
+      <Link to="#" onClick={(e) => switchTheme(e)}>
+        <img src={imgMoon} alt="" />
+      </Link>
     </div>
-    );
-}
+  );
+};
 
 export default DarkMode;
