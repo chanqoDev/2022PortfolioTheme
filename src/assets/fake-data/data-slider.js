a06f61cc6bd992a5e46462062c7b359fef0a8595
Christian E <christianelena92@gmail.com>
2022-07-06 05:17:32 -0500

    update: slider slide png

--- src/assets/fake-data/data-slider.js
+++ src/assets/fake-data/data-slider.js
@@ -1,38 +1,39 @@
 import img1 from '../images/slider/slide_1.png';
 import img2 from '../images/slider/slide_1.png';
-import imgbg1 from '../images/slider/bg_slide_1.png'
-import imgbg2 from '../images/slider/bg_slide_1.png'
-
+import imgbg1 from '../images/slider/bg_slide_1.png';
+import imgbg2 from '../images/slider/bg_slide_1.png';
 
 const heroSliderData = [
-    {
-        title_1: "Discover, find,",
-        title_2: "Sell extraordinary",
-        title_3: "Monster NFTs",
-        description: "Marketplace for monster character cllections non fungible token NFTs",
-        img: img1,
-        imgbg: imgbg1,
-        class:'left'
-    },
-    {
-        title_1: "Discover, find,",
-        title_2: "Sell extraordinary",
-        title_3: "Monster NFTs",
-        description: "Marketplace for monster character cllections non fungible token NFTs",
-        img: img2,
-        imgbg: imgbg2,
-        class:'center'
-    },
-    {
-        title_1: "Discover, find,",
-        title_2: "Sell extraordinary",
-        title_3: "Monster NFTs",
-        description: "Marketplace for monster character cllections non fungible token NFTs",
-        img: img2,
-        imgbg: imgbg2,
-        class:'right'
-    },
-    
-]
+  {
+    title_1: 'Christian A. Elena',
+    title_2: 'Software Developer & Website Designer',
+    title_3: '2022 Portfolio',
+    description:
+      'Find my latest work and Projects utilizing the latest technologies',
+    img: img1,
+    imgbg: imgbg1,
+    class: 'left',
+  },
+  {
+    title_1: 'Discover, Find,',
+    title_2: 'Extraordinary Talent',
+    title_3: 'Previous Work & Projects ',
+    description:
+      'Find my latest work and Projects implemented with best practices',
+    img: img2,
+    imgbg: imgbg2,
+    class: 'center',
+  },
+  //   {
+  //     title_1: 'Discover, find,',
+  //     title_2: 'Sell extraordinary',
+  //     title_3: 'Monster NFTs',
+  //     description:
+  //       'Marketplace for monster character cllections non fungible token NFTs',
+  //     img: img2,
+  //     imgbg: imgbg2,
+  //     class: 'right',
+  //   },
+];
 
 export default heroSliderData;
